1036217c95b3ed5d30a368a1d9975caacb85bc50
2022148084 <cheesetime@yonsei.ac.kr>
2023-11-21 23:58:43 +0900

    fix typo

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-
         <span className="orange_gradient text center">Songjuk Post-it</span>
       </h1>
       <p className='desc text-center'>
