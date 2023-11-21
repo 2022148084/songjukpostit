6d284379c7905dc9c43babf2613f3b3b90d42b06
2022148084 <cheesetime@yonsei.ac.kr>
2023-11-22 01:58:53 +0900

    fix typo

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -35,7 +35,7 @@ const Feed = () => {
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  },[session?.user.id]);
 
   const filterPrompts = (searchtext) => {
     const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
